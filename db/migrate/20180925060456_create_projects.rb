65cf3b0d34bb2dde7114809b1cf818f57f43baec
Annie Shin <annieyshin@gmail.com>
2018-09-24 23:07:28 -0700

    add projects migration and generate devise user

---
+++ db/migrate/20180925060456_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :projects do |t|
+      t.column :title, :string
+      t.column :description, :string
+      t.column :url, :string
+
+
+      t.timestamps
+    end
+  end
+end
